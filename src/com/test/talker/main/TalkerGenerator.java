216ef6bdfda1494d7ba8cd30b63d7881c3182321
k28254 <sudipta.mukhopadhyay@finra.org>
2022-08-10 00:38:01 -0400

    Code Solution 08-10-2022

--- src/com/test/talker/main/TalkerGenerator.java
+++ src/com/test/talker/main/TalkerGenerator.java
@@ -43,34 +43,53 @@ public class TalkerGenerator {
 
 	/**
 	 * get the top talkers
-	 * Time Complexity
-	 *
-	 *
-	 *
+	 * Time Complexity is the most time-consuming of the granular operations inside the method
+	 * Time Complexity here is O(NlogN) for the sorting process
+	 * Best Case Time Complexity possibility here is O(N)
+	 * Space complexity is the amount of memory used for operations which is proportional
+	 * to the values stored in the list which will grow linear
+	 * Space Complexity = O(N)
 	 */
 	public List<TopTalker> getTopTalker(int n) {
 
 		List<TopTalker> topTalkers = new ArrayList<>();
 		
 		Map<String, TopTalker> talkCounts = new HashMap<>();
-		
+
+		//Looping through the chat messages list is proportional to the length of the list
+		//Time Complexity O(N)
+		//Space Complexity is O(N)
 		for(ChatMessage cm : chatMessages) {
 			String talkUser = cm.getUser();
+			// Hashmap get/search a constant time operation O(1)
+			// The Worst Case O(N) = Very rarely happens we can safely ignore
 			TopTalker talker = talkCounts.get(talkUser);
+			//If talker is found yet in the HashMap
 			if(talker != null) {
 				talker.addOneCountForUser();
+				// Hashmap put a constant time operation O(1)
+				// Space complexity is O(1)
 				talkCounts.put(talkUser, talker);
 			}else {
+				//If talker is not found in the HashMap create a fresh new and insert into the Map
 				TopTalker newTalker = new TopTalker(talkUser);
 				newTalker.addOneCountForUser();
+				// Hashmap put a constant time operation O(1)
+				// Space complexity is O(1)
 				talkCounts.put(talkUser, newTalker);				
 			}
 		}
-		
+		//Create a list of Value objects from the Map Values with final message counts
 		List<TopTalker> talkerList = new ArrayList<>(talkCounts.values());
-		
+
+		//Sort the list of Talkers so that we can find the top talkers by just
+		//picking up the subset of the list till that value
+		//Efficient sorting algorithm will be of time complexity O(NlogN)
 		talkerList.sort(new SortbyMessageCount());
-		
+
+		//Ignore any value of n as zero or less than zero as zero means pick up the top zero
+		//Negative value of n is irrational in that case return empty list
+		//Time Complexity Insert/Remove from a list from its beginning is O(1) operation
 		if(n>0) {
 			topTalkers = talkerList.subList(0, Math.min(n, talkerList.size()));
 		}
@@ -80,10 +99,16 @@ public class TalkerGenerator {
 
 }
 
+/**
+ * Final class of a Comparator to sort by message count in descending order
+ */
 final class SortbyMessageCount implements Comparator<TopTalker> {
-	  
-    // Used for sorting in descending order of
-    // roll number
+
+	/**
+	 * Used for sorting in descending order of message count
+	 * @param a the first object to be compared.
+	 * @param b the second object to be compared.
+	 */
     public int compare(TopTalker a, TopTalker b)
     {
         return b.getMessageCount() - a.getMessageCount();
--- src/com/test/talker/main/TalkerGeneratorTest.java
+++ src/com/test/talker/main/TalkerGeneratorTest.java
@@ -1,6 +1,22 @@
 /**
- * 
- */
+ * Pre-Assessment Question
+ * You are part of a team working on a new chat application. Your task is to
+ * provide a way to generate the top n talkers from a list of messages. Top talkers are
+ * users with the most number of ChatMessages.
+ *
+ * Requirements:
+ *   1. TopTalkerGenerator class must have a way to ingest List<ChatMessage>
+ *   2. TopTalkerGenerator class must have a way to output n top talkers e.g. List<TopTalker>
+ *   3. Please add comments about space and time complexity of the solution above TopTalkerGenerator class
+ *
+ * Example:
+ * Given chat messages from users
+ * [userA, userA, userA, userB, userC, userC]
+ * userA messageCount = 3
+ * userB messageCount = 1
+ * userC messageCount = 2
+ * The top two talkers (n = 2) would be [userA, userC]
+ **/
 package com.test.talker.main;
 
 import java.util.ArrayList;
@@ -17,11 +33,15 @@ public class TalkerGeneratorTest {
 
 
 	/**
+	 * We need to run the main method to run the tests
 	 */
 	public static void main(String[] args) {
 		System.out.println("Started Main Method");
 		List<ChatMessage> inputChatMessages = new ArrayList<>();
-		
+
+		//Input Chat messages with below message counts
+		// Data Scenarios created to handle multiple candidates with same count values
+		// A = 5; B = 2; C = 2; D = 5; E = 1
 		ChatMessage cm1 = new ChatMessage("A", "I am A");
 		ChatMessage cm2 = new ChatMessage("B", "I am B");
 		ChatMessage cm3 = new ChatMessage("A", "I am A");
@@ -38,6 +58,7 @@ public class TalkerGeneratorTest {
 		ChatMessage cm14 = new ChatMessage("D", "I am D");
 		ChatMessage cm15 = new ChatMessage("A", "I am A");
 
+		//Populating the List of Chat messages
 		inputChatMessages.add(cm1);
 		inputChatMessages.add(cm2);
 		inputChatMessages.add(cm3);
@@ -54,17 +75,72 @@ public class TalkerGeneratorTest {
 		inputChatMessages.add(cm14);
 		inputChatMessages.add(cm15);
 		
-		System.out.println("Calling Talker generator");
+		System.out.println("Setting up the Talker generator Object");
 		TalkerGenerator gen = new TalkerGenerator(inputChatMessages);
-		int n = 1;
-		System.out.println("Getting top " + n + " talkers..");
-		List<TopTalker> top = gen.getTopTalker(n);
-		
-		System.out.println("Received list:" + top.size());
-		
-		top.forEach(a -> System.out.print(a.toString()));
-		
 
+		System.out.println("Start tests..");
+		//Running the test methods for different scenarios
+		testForTopThree(gen);
+		testForTopOne(gen);
+		testForTopTen(gen);
+		testForTopZero(gen);
+		testForTopNegativeValue(gen);
+
+		System.out.println("End Tests..");
 	}
 
+	/**
+	 * Positive Test for top three
+	 * @param gen The populated TalkerGenerator Object
+	 */
+	private static void testForTopThree(TalkerGenerator gen) {
+		int n = 3;
+		printContent(gen, n);
+	}
+
+	/**
+	 * Positive Test for top One
+	 * @param gen The populated TalkerGenerator Object
+	 */
+	private static void testForTopOne(TalkerGenerator gen) {
+		int n = 1;
+		printContent(gen, n);
+	}
+
+	/**
+	 * Test for top ten - Negative test when there is less than 10 talkers
+	 * @param gen The populated TalkerGenerator Object
+	 */
+	private static void testForTopTen(TalkerGenerator gen) {
+		int n = 10;
+		printContent(gen, n);
+	}
+
+	/**
+	 * Test for top zero - Negative test when zero value is passed
+	 * @param gen The populated TalkerGenerator Object
+	 */
+	private static void testForTopZero(TalkerGenerator gen) {
+		int n = 0;
+		printContent(gen, n);
+	}
+
+	/**
+	 * Negative test when negative value is passed
+	 * @param gen The populated TalkerGenerator Object
+	 */
+	private static void testForTopNegativeValue(TalkerGenerator gen) {
+		int n = -3;
+		printContent(gen, n);
+	}
+
+	private static void printContent(TalkerGenerator gen, int n) {
+		System.out.println("Getting top " + n + " talkers..");
+		List<TopTalker> top = gen.getTopTalker(n);
+
+		System.out.println("Received list:" + top.size());
+
+		top.forEach(a -> System.out.println(a.toString()));
+		System.out.println("\n");
+	}
 }
